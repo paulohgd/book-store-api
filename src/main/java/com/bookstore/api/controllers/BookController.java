66b096717ee78dddb970f42ae3a7777e1020d05d
Paulo Henrique <paulohguerra@outlook.com.br>
2024-04-17 00:33:04 -0300

    Criamos um construtor do nosso BookController e já estamos no desenvolvimneto da listagem dos livors.

--- src/main/java/com/bookstore/api/controllers/BookController.java
+++ src/main/java/com/bookstore/api/controllers/BookController.java
@@ -1,16 +1,27 @@
 package com.bookstore.api.controllers;
+import com.bookstore.api.domain.Book;
+import com.bookstore.api.feign.BookClient;
+import com.bookstore.api.services.BookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/books")
 public class BookController {
 
+    private final BookService service;
+
+    public BookController(BookService service) {
+        this.service = service;
+    }
 
     @GetMapping()
     public String getBooks(){
-        return "oi";
+        List<Book> books = this.service.getBooksFromLibrary();
+        return "Oi";
     }
 
 }
